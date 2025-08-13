e32cc1a83d5125ea1f18daf07997704fe9890a9d
idn-fahmifn <fahmifn.idn@gmail.com>
2025-08-13 12:00:19 +0700

    3.4

---
+++ javascript/app.js
@@ -0,0 +1,34 @@
+function hello(){
+    alert("Hallo saya sedang belajar javascript")
+}
+
+// Variable
+let mobil = 'Honda Jazz'
+mobil = 'Panther'
+console.log(mobil)
+
+const lahir = '30 Februari 2001'
+const Lahir = '20 Februari 2001'
+console.log(Lahir)
+
+// Tipe data
+// Primitif
+// 1. String
+
+const rumah = "Kemannggisan"
+let jalan = 'Jalan Palmerah I'
+let myHome = `Rumah saya berada di ${rumah}, nama jalannya adalah ${jalan}`
+
+console.log('Saya punya rumah alamatnya di ' + rumah + "Nama jalannya adalah " +jalan)
+
+console.log(`Rumah saya di ${rumah}, nama jalannya adalah ${jalan}`)
+console.log(myHome)
+
+const angka1 = 12345
+let dec = 0.3
+const sisi2 = 10
+let ten = '10'
+console.log(sisi2 + sisi2)
+
+
+
